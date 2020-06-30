ae860c20a55c0ea83a590b1cfa897699a4f29093
Hao Fang <haofang1990@gmail.com>
2020-06-30 12:06:47 -0700

    fine

--- acl2020_tools/utils/process_zoom_schedule_with_links.py
+++ acl2020_tools/utils/process_zoom_schedule_with_links.py
@@ -5,15 +5,15 @@ import pandas as pd
 
 def main(schedule_csv: str, outbase: str) -> None:
     schedule_df = pd.read_csv(
-        schedule_csv,
-        sep=',',
-        encoding='utf-8',
-        na_values=None,
-        keep_default_na=False
+        schedule_csv, sep=",", encoding="utf-8", na_values=None, keep_default_na=False
     )
     schedule_df.loc[:, "UID"] = schedule_df.loc[:, "id"]
-    schedule_df.loc[:, 'session_name'] = schedule_df.loc[:, "uniqueid"].apply(lambda x: x.split('.')[-1])
-    schedule_df.loc[:, "zoom_join_link"] = schedule_df.loc[:, "join_link(for Attendees)"]
+    schedule_df.loc[:, "session_name"] = schedule_df.loc[:, "uniqueid"].apply(
+        lambda x: x.split(".")[-1]
+    )
+    schedule_df.loc[:, "zoom_join_link"] = schedule_df.loc[
+        :, "join_link(for Attendees)"
+    ]
 
     colnames = [
         "UID",
@@ -25,35 +25,18 @@ def main(schedule_csv: str, outbase: str) -> None:
     ]
 
     schedule_df.to_csv(
-        outbase + ".csv",
-        sep=',',
-        index=False,
-        encoding='utf-8',
-        columns=colnames
+        outbase + ".csv", sep=",", index=False, encoding="utf-8", columns=colnames
     )
 
-    schedule_df.to_excel(
-        outbase + ".xlsx",
-        index=False,
-        columns=colnames
-    )
+    schedule_df.to_excel(outbase + ".xlsx", index=False, columns=colnames)
 
 
-if __name__ == '__main__':
-    cmdline_parser = argparse.ArgumentParser(
-        description=__doc__
-    )
-    cmdline_parser.add_argument(
-        '--schedule_csv',
-        help='input zoom schedule csv'
-    )
-    cmdline_parser.add_argument(
-        '--outbase',
-        help='output files basename'
-    )
+if __name__ == "__main__":
+    cmdline_parser = argparse.ArgumentParser(description=__doc__)
+    cmdline_parser.add_argument("--schedule_csv", help="input zoom schedule csv")
+    cmdline_parser.add_argument("--outbase", help="output files basename")
     args = cmdline_parser.parse_args()
 
     main(
-        schedule_csv=args.schedule_csv,
-        outbase=args.outbase,
+        schedule_csv=args.schedule_csv, outbase=args.outbase,
     )
