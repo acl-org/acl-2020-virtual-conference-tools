cae0524a1aaa0c0155bfa2aee61e3fa998d689d5
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-07-01 16:12:52 -0300

    Add owners to channels list

--- acl2020_tools/chat/list_channels.py
+++ acl2020_tools/chat/list_channels.py
@@ -23,6 +23,12 @@ def parse_arguments():
         default="./channels.csv",
         help="Output csv filepath. Default: ./channels.csv",
     )
+
+    parser.add_argument(
+        "--add_owners",
+        default=False,
+        help="Add owners of each channel. Default: False",
+    )
     parser.add_argument(
         "--regexp",
         "--regex",
@@ -59,6 +65,26 @@ def postprocess(channels: pd.DataFrame) -> pd.DataFrame:
 
     return channels
 
+def add_owners(channels: pd.DataFrame, rocket: RocketChat) -> pd.DataFrame:
+    channels["owners"] = None
+
+    for idx, row in tqdm(channels.iterrows(), total=len(channels)):
+        if row.name:
+            ret = rocket.channels_roles(room_id=row._id)
+            if ret.status_code == 200:
+                # Success! => add the owners
+                owners = []
+                user_roles = ret.json()["roles"]
+
+                for user_role in user_roles:
+                    if 'owner' in user_role["roles"]:
+                        owners.append(user_role["u"]["username"])
+
+                channels.loc[idx, "owners"] = ",".join(owners)
+            else:
+                print(f"Problem retrieving {ret.name}")
+                print(ret.status_code, ret.reason)
+    return channels
 
 def get_params(filter_featured: bool = False, regexp: str = None) -> Dict[str, str]:
     # t: channel type (d: Direct chat, c: Chat, p: Private chat, l: Livechat)
@@ -106,4 +132,8 @@ if __name__ == "__main__":
             channels_list.extend(channels_json["channels"])
 
         channels_df = postprocess(pd.DataFrame(channels_list))
+
+        if args.add_owners:
+            print("Adding owners")
+            channels_df = add_owners(channels_df, rocket)
         channels_df.to_csv(args.output_file, index=False)
